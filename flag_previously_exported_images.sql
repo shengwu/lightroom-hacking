9daf0b99ee5c9593ced16ff85cca377a487c4f7d
Sheng Wu <shengwu34@gmail.com>
2015-12-26 01:49:37 -0600

    add alternate queries

--- flag_previously_exported_images.sql
+++ flag_previously_exported_images.sql
@@ -18,3 +18,17 @@ WHERE id_local IN
 (SELECT image.id_local FROM Adobe_images AS image
 JOIN Adobe_libraryImageDevelopHistoryStep AS step
 WHERE image.id_local = step.image AND step.name LIKE "Export%");
+
+-- Alternate queries for working with older lightroom catalogs
+-- Count images that have been previously edited
+SELECT COUNT(DISTINCT image.id_local) FROM Adobe_images AS image
+JOIN Adobe_libraryImageDevelopHistoryStep AS step
+WHERE image.id_local = step.image AND step.name NOT LIKE "Import%" AND step.name <> "Synchronize Settings";
+
+-- Flag all images that have been previously edited
+UPDATE Adobe_images
+SET pick = 1.0
+WHERE id_local IN
+(SELECT DISTINCT image.id_local FROM Adobe_images AS image
+JOIN Adobe_libraryImageDevelopHistoryStep AS step
+WHERE image.id_local = step.image AND step.name NOT LIKE "Import%" AND step.name <> "Synchronize Settings");
